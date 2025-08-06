9600338535004c601f4c1390019517349951ee43
RedLeader962 <redleader962@gmail.com>
2025-08-05 21:49:02 -0400

    feat: enhance compatibility and add user-defined logic examples
    - Add MacOS GID conversion note in `load_super_project_config.bash` to address
      critical compatibility issue and plan future fix (NMO-773).
    - Update pip to the latest version for improved dependency management.
    - Add Jetson architecture-specific logic examples in callback scripts to guide
      users.
    - Enhance documentation formatting for better readability and clarity.
    
    Issue NMO-773, NMO-770

--- .dockerized_norlab/configuration/project_entrypoints/project-develop/dn_entrypoint.init.callback.bash
+++ .dockerized_norlab/configuration/project_entrypoints/project-develop/dn_entrypoint.init.callback.bash
@@ -16,3 +16,10 @@ source /dna-lib-container-tools/project_entrypoints/entrypoint_helper.common.bas
 
 # ====DN-project user defined logic================================================================
 # Add your code here
+
+# ....Example......................................................................................
+if [[ $( n2st::which_architecture_and_os ) == "l4t\arm64" ]]; then
+  n2st::print_msg "Is running on a Jetson..."
+  # Add Jetson logic e.g., cat /proc/device-tree/model
+fi
+# .................................................................................................
