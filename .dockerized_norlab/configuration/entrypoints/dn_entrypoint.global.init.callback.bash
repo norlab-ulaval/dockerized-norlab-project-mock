f4f75a8890e95dc838d27ffd8ea0ef4da8d9eae2
RedLeader962 <redleader962@gmail.com>
2025-11-08 14:03:21 -0500

    refactor: rename "project_entrypoints" and "project_requirements" directories
    - Changed directory names to better reflect their purposes:
      - Renamed "project_entrypoints" to "entrypoints".
      - Renamed "project_requirements" to "build_stage".
    - Updated all file references and scripts to match the new directory names.
    - Improves code organization and naming consistency across the project.
    - No functional changes introduced.
    
    Issue NMO-794

--- .dockerized_norlab/configuration/entrypoints/dn_entrypoint.global.init.callback.bash
+++ .dockerized_norlab/configuration/entrypoints/dn_entrypoint.global.init.callback.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 # =================================================================================================
 # Dockerized-NorLab container runtime entrypoint callback.
-# Is executed each time a shell is attach to a project-develop or project-deploy container.
+# Is executed only once on container initialisation, at the end of the project-develop or
+# project-deploy entrypoints.
 #
 # Usage:
-#   Add project wide logic that need to be executed by each shell.
+#   Add project wide logic that need to be executed only on startup.
 #
 # Globals:
 #   Read/write all environment variable exposed in DN at runtime
@@ -12,10 +13,17 @@
 # =================================================================================================
 
 # ....DNA-project internal logic...................................................................
-source /dna-lib-container-tools/project_entrypoints/entrypoint_helper.global.common.bash || exit 1
+source /dna-lib-container-tools/entrypoints/entrypoint_helper.global.common.bash || exit 1
+source /dna-lib-container-tools/entrypoints/entrypoint_helper.global.init.bash || exit 1
 
 # ====DNA-project user defined logic===============================================================
 # Add your code here
+
+# ....DNA-project optional logic...................................................................
+source /dna-lib-container-tools/entrypoints/entrypoint_helper.show_info.bash || exit 1
+
+# To check available N2ST lib and DN lib functions, un-comment the following lines
+dn::show_dn_and_n2st_available_functions
 
 # ....Examples: source ROS2 environment variables..................................................
 #dn::source_ros2_underlay_only
